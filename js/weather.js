92ec52328a9c58510f55dac0ac85159ebf99191d
201250104 <2681299706@qq.com>
2022-02-16 15:25:15 +0800

    Site updated: 2022-02-16 15:25:15

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "125",
+    "top": "10",
+    "key": "81b4691c797d430b887a8b49021454ba"
+  }
+}
